0ae9b61e6057d7da89379182ede58a42094c0344
maxim <n0220po12@gmail.com>
2025-03-28 16:47:47 -0400

    second

--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,5 @@
-from app.main import app
+import uvicorn
 
-# Запускається через: uvicorn run:app --reload
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+
