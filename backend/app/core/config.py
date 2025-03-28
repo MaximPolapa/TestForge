40aeabc706de423eda9d3903cd21e8de928b9b9d
maxim <n0220po12@gmail.com>
2025-03-28 16:34:37 -0400

    second

---
+++ backend/app/core/config.py
@@ -0,0 +1,10 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "TestForge"
+    DATABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
---
+++ backend/run.py
@@ -0,0 +1,3 @@
+from app.main import app
+
+# Запускається через: uvicorn run:app --reload
